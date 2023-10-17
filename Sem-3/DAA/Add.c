a95154d0b051476de650e3f903a7392a5be17064
dmb369 <116587999+dmb369@users.noreply.github.com>
2023-10-17 12:22:01 +0530

    Update Add.c

--- Sem-3/DAA/Add.c
+++ Sem-3/DAA/Add.c
@@ -5,11 +5,35 @@
 void add(int x,int y, int n)
 {
     int z=x+y;
-    int a=z%n;
-    printf("modN value is %d",a);
+    int r;
+    
+    if (z==0)
+    {
+        r=0;
+    }
+    
+    int l=x/2;
+    
+    int b=l/n;
+    r=b%n;
+
+    r=2*r;
+    
+    if (z%2!=0)
+    {
+        r=r+1;
+        if (r>=n)
+        {
+            r-=n;
+        }
+    }
+    
+    if (r==n) r=0;
+    
+    printf("modN value is %d",r);
 }
 
-int main() 
+int main()
 {
     int x,y;
     scanf("%d %d",&x,&y);
