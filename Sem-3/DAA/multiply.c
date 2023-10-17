fdc8a1adaf38acd400fe97d4aadc84ebcb0e7e35
dmb369 <116587999+dmb369@users.noreply.github.com>
2023-10-17 12:22:39 +0530

    Update multiply.c

--- Sem-3/DAA/multiply.c
+++ Sem-3/DAA/multiply.c
@@ -5,11 +5,37 @@
 void mult(int x,int y, int n)
 {
     int z=x*y;
-    int a=z%n;
-    printf("modN value is %d",a);
+    
+    int r;
+        
+    if (z==0)
+    {
+        r=0;
+    }
+        
+    int l=z/2;
+    r=l%n;
+
+    r=2*r;
+        
+    if (z%2!=0)
+    {
+        r=r+1;
+        if (r>=n)
+        {
+            r-=n;
+        }
+    }
+    
+    if (r==n)
+    {
+        r=0;
+    }
+        
+    printf("modN value is %d",r);
 }
 
-int main() 
+int main()
 {
     int x,y;
     scanf("%d %d",&x,&y);
